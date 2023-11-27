96e97017e61581060a657441627e4125e371d46a
Nathan Mascarenhas <nathan.andrade@csptech.com.br>
2023-11-27 19:50:59 -0300

    Criação de classes e metódos

--- Ambiente de desenvolvimento de software/src/App.java
+++ Ambiente de desenvolvimento de software/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Operações op = new Operações();
+        int soma = op.add(5, 3);
+        int subtração = op.subtract(10, 4);
+
+        System.out.println("Resultado da soma: " + soma);
+        System.out.println("Resultado da subtração: " + subtração);
     }
 }
